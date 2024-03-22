528d47a96aba74d0e0979be9243945e56d34cc6f
Ghostm7 <150959507+Ghostm7@users.noreply.github.com>
2024-03-22 16:14:39 +0300

    Added hash password to regModel.js

--- node.js/studentapi/Model/regModel.js
+++ node.js/studentapi/Model/regModel.js
@@ -23,5 +23,23 @@ module.exports = (sequelize, DataTypes) => {
 
 
     // Function to hash password before saving
-    
+    Reg.beforeCreate( async (user) => {
+        try {
+            const salt = await bcrypt.genSalt(12);
+            const hashedPwd = await bcrypt.hash(user.regPassword, salt)
+            user.regPassword = hashedPwd;
+        } catch (error) {
+            throw new Error("Error encrypting password")
+        }
+    })
+
+    // Function to compare the entered password with the saved hashed password
+    Reg.prototype.isValidPassword = async function(password) {
+        try {
+            return await bcrypt.compare(password, this.password);
+        } catch (error) {
+            throw error;
+        }
+    };
 }
+    
