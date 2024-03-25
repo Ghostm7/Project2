2c182075d003dd149a5b80eb6888dec8e1c244e9
Ghostm7 <150959507+Ghostm7@users.noreply.github.com>
2024-03-25 04:53:04 +0300

    Added the hash password

--- node.js/studentapi/Model/regModel.js
+++ node.js/studentapi/Model/regModel.js
@@ -1,3 +1,51 @@
+// module.exports = (sequelize, DataTypes) => {
+//     const Reg = sequelize.define('registration', {
+//         reg_id: {
+//             type: DataTypes.INTEGER,
+//             primaryKey: true,
+//             autoIncrement: true,
+//         },
+//         regName: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         },
+//         regEmail: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         },
+//         regPassword: {
+//             type: DataTypes.STRING,
+//             allowNull: false,
+//         },
+//     });
+    
+//     return Reg;
+
+
+//     // Function to hash password before saving
+//     Reg.beforeCreate( async (user) => {
+//         try {
+//             const salt = await bcrypt.genSalt(12);
+//             const hashedPwd = await bcrypt.hash(user.regPassword, salt)
+//             user.regPassword = hashedPwd;
+//         } catch (error) {
+//             throw new Error("Error encrypting password")
+//         }
+//     })
+
+//     // Function to compare the entered password with the saved hashed password
+//     Reg.prototype.isValidPassword = async function(password) {
+//         try {
+//             return await bcrypt.compare(password, this.password);
+//         } catch (error) {
+//             throw error;
+//         }
+//     };
+// }
+    
+
+const bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
     const Reg = sequelize.define('registration', {
         reg_id: {
@@ -17,29 +65,29 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false,
         },
+    }, {
+        hooks: {
+            // Hook before creating a new instance of the Reg model
+            beforeCreate: async (reg) => {
+                try {
+                    const salt = await bcrypt.genSalt(12);
+                    const hashedPwd = await bcrypt.hash(reg.regPassword, salt);
+                    reg.regPassword = hashedPwd;
+                } catch (error) {
+                    throw new Error("Error encrypting password");
+                }
+            }
+        }
     });
     
-    return Reg;
-
-
-    // Function to hash password before saving
-    Reg.beforeCreate( async (user) => {
-        try {
-            const salt = await bcrypt.genSalt(12);
-            const hashedPwd = await bcrypt.hash(user.regPassword, salt)
-            user.regPassword = hashedPwd;
-        } catch (error) {
-            throw new Error("Error encrypting password")
-        }
-    })
-
     // Function to compare the entered password with the saved hashed password
     Reg.prototype.isValidPassword = async function(password) {
         try {
-            return await bcrypt.compare(password, this.password);
+            return await bcrypt.compare(password, this.regPassword);
         } catch (error) {
             throw error;
         }
     };
-}
-    
+
+    return Reg;
+};
