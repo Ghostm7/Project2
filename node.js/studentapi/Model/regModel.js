d5026ec167acfdea6b60c7afe1ba16732d15678f
Ghostm7 <150959507+Ghostm7@users.noreply.github.com>
2024-03-25 12:44:09 +0300

    Added a generateKeys

--- node.js/studentapi/Model/regModel.js
+++ node.js/studentapi/Model/regModel.js
@@ -1,49 +1,3 @@
-// module.exports = (sequelize, DataTypes) => {
-//     const Reg = sequelize.define('registration', {
-//         reg_id: {
-//             type: DataTypes.INTEGER,
-//             primaryKey: true,
-//             autoIncrement: true,
-//         },
-//         regName: {
-//             type: DataTypes.STRING,
-//             allowNull: false,
-//         },
-//         regEmail: {
-//             type: DataTypes.STRING,
-//             allowNull: false,
-//         },
-//         regPassword: {
-//             type: DataTypes.STRING,
-//             allowNull: false,
-//         },
-//     });
-    
-//     return Reg;
-
-
-//     // Function to hash password before saving
-//     Reg.beforeCreate( async (user) => {
-//         try {
-//             const salt = await bcrypt.genSalt(12);
-//             const hashedPwd = await bcrypt.hash(user.regPassword, salt)
-//             user.regPassword = hashedPwd;
-//         } catch (error) {
-//             throw new Error("Error encrypting password")
-//         }
-//     })
-
-//     // Function to compare the entered password with the saved hashed password
-//     Reg.prototype.isValidPassword = async function(password) {
-//         try {
-//             return await bcrypt.compare(password, this.password);
-//         } catch (error) {
-//             throw error;
-//         }
-//     };
-// }
-    
-
 const bcrypt = require('bcrypt');
 
 module.exports = (sequelize, DataTypes) => {
@@ -67,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
         },
     }, {
         hooks: {
-            // Hook before creating a new instance of the Reg model
+            // Hook before creating an instance of the model
             beforeCreate: async (reg) => {
                 try {
                     const salt = await bcrypt.genSalt(12);
